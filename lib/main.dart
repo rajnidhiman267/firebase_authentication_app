e5e6c52cb0623a85cdf359c11b2c1b9f91709f1d
Rajni Dhiman <rajni@nestorbird.com>
2025-06-17 23:18:42 +0530

    add textfield

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,14 @@ import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-await NotificationService.instance.getDeviceToken();
+  firebaseInit();
 
   runApp(const MyApp());
+}
+
+void firebaseInit() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await NotificationService.instance.getDeviceToken();
 }
 
 class MyApp extends StatelessWidget {
@@ -17,35 +21,176 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(title: 'Flutter Demo', home: const MyHomePage());
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late TextEditingController emailController, passwordController;
+
+  final _formKey = GlobalKey<FormState>();
+  bool isValid = false;
+  bool isObscure = true;
+  @override
+  void initState() {
+    super.initState();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      backgroundColor: Colors.pink.shade50,
+      body: Form(
+        key: _formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16),
 
-        title: Text(widget.title),
+              child: TextFormField(
+                controller: emailController,
+                validator: (value) {
+                  if (value == null || value.isEmpty || !value.contains('@')) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  // Validate and refresh state
+                  setState(() {
+                    isValid = _formKey.currentState?.validate() ?? false;
+                  });
+                },
+                decoration: InputDecoration(
+                  prefixIconConstraints: BoxConstraints(
+                    maxWidth: 200,
+                    minWidth: 60,
+                  ),
+
+                  prefixIcon: Icon(Icons.person, color: Colors.grey),
+                  suffixIcon: AnimatedSwitcher(
+                    duration: Duration(milliseconds: 300),
+                    transitionBuilder: (child, animation) =>
+                        FadeTransition(opacity: animation, child: child),
+                    child: isValid
+                        ? Icon(
+                            Icons.check_circle_rounded,
+                            color: Colors.green,
+                            key: ValueKey('valid'),
+                          )
+                        : SizedBox(
+                            key: ValueKey('invalid'),
+                            width: 0,
+                            height: 0,
+                          ),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white70),
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 1),
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+
+                  errorStyle: TextStyle(
+                    color: Colors.red,
+
+                    fontWeight: FontWeight.w400,
+                  ),
+                  // Reserve space for error
+                  helperText: ' ',
+                  helperStyle: TextStyle(height: 1.5),
+
+                  focusedErrorBorder: OutlineInputBorder(
+                    gapPadding: 0.0,
+                    borderSide: BorderSide(color: Colors.red, width: 1),
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+
+                  hintText: "abc@gmail.com",
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              child: TextFormField(
+                controller: passwordController,
+                validator: (value) {
+                  if (value == null || value.isEmpty || value.length < 6) {
+                    return 'Password must be at least 6 letters';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    isValid = _formKey.currentState?.validate() ?? false;
+                  });
+                },
+                obscureText: isObscure,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.lock, color: Colors.grey),
+
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  hintText: "Enter password",
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w300,
+                  ),
+                  errorStyle: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  suffixIcon: InkWell(
+                    onTap: () {
+                      setState(() {
+                        isObscure = !isObscure;
+                      });
+                    },
+                    child: Icon(
+                      isObscure ? Icons.visibility_off : Icons.visibility,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Center(child: Text("data")),
     );
   }
 }
